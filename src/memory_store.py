48fc4f7ad1d461373c58dde2c0d1a7492c81bf93
Janaki Radhakrishnan <janaki26794@gmail.com>
2026-09-24 23:13:24 +0100

    feat: add memory store

---
+++ src/memory_store.py
@@ -0,0 +1,24 @@
+from src.memory import Memory
+
+
+class MemoryStore:
+    def __init__(self):
+        self.memories = []
+
+    def add(self, memory: Memory):
+        self.memories.append(memory)
+
+    def get_all(self):
+        return self.memories
+
+    def get_by_id(self, memory_id: str):
+        for memory in self.memories:
+            if memory.id == memory_id:
+                return memory
+        return None
+
+    def delete(self, memory_id: str):
+        self.memories = [
+            memory for memory in self.memories
+            if memory.id != memory_id
+        ]
